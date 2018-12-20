a1796995f246f67b6ce748a5180c1e552aae8318
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-20 04:45:01 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:user][:name])
     return head(:forbidden) unless @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
-    
+    render 'home'
   end
 
   def home
